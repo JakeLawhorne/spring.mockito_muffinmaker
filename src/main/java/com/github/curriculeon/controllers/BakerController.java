e59f80e68e9d39cb61788138bb2caa4218ce52ae
JakeLawhorne <jake.lawhorne@gmail.com>
2024-02-28 01:27:08 -0500

    finished annotations

--- src/main/java/com/github/curriculeon/controllers/BakerController.java
+++ src/main/java/com/github/curriculeon/controllers/BakerController.java
@@ -2,32 +2,43 @@ package com.github.curriculeon.controllers;
 
 import com.github.curriculeon.services.BakerService;
 import com.github.curriculeon.models.Baker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping(value = "/baker")
 public class BakerController {
     private BakerService service;
 
+    @Autowired
     public BakerController(BakerService service) {
         this.service = service;
     }
 
+    @GetMapping(value = "/readAll")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/read/{id}")
     public ResponseEntity<Baker> show(Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
+    @PostMapping(value="/create")
     public ResponseEntity<Baker> create(Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
+    @PutMapping(value = "/update")
     public ResponseEntity<Baker> update(Long id, Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
+    @DeleteMapping(value = "/delete")
     public ResponseEntity<Boolean> destroy(Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
--- src/main/java/com/github/curriculeon/controllers/MuffinController.java
+++ src/main/java/com/github/curriculeon/controllers/MuffinController.java
@@ -2,32 +2,43 @@ package com.github.curriculeon.controllers;
 
 import com.github.curriculeon.models.Muffin;
 import com.github.curriculeon.services.MuffinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping(value = "/muffin")
 public class MuffinController {
     private MuffinService service;
 
+    @Autowired
     public MuffinController(MuffinService service) {
         this.service = service;
     }
 
+    @GetMapping(value = "/readAll")
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/read/{id}")
     public ResponseEntity<Muffin> show(Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
+    @PostMapping(value = "/create")
     public ResponseEntity<Muffin> create(Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
+    @PutMapping(value = "/update")
     public ResponseEntity<Muffin> update(Long id, Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
+    @DeleteMapping(value = "/delete")
     public ResponseEntity<Boolean> destroy(Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
